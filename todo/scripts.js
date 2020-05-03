8513650b4d8ac630ed2355b9b4162721d8720058
drdoky <drdoky@users.noreply.github.com>
2020-05-04 00:26:28 +0200

    progress

--- todo/scripts.js
+++ todo/scripts.js
@@ -1,16 +1,19 @@
-let strtodo;
-
-$("#btnTodo").on('click', () => {
-  strtodo = $("#inTodo").val();
+ function addTodo (strtodo) {
+  if (typeof strtodo !== "string") {
+    strtodo = $("#inTodo").val();
+  }
   if ((typeof strtodo === "string") && (strtodo != "")) {
-    //$("#todoList").append("<li class='todoItem'>" + strtodo + "</li>");
     $("#todoList").append("<li>" + strtodo + "</li>");
     $("li:last-child").toggleClass("todoItem");
-    //$("li:last-child").click();
     $("#inTodo").val("");
+    $("#inTodo").focus();
   }
-});
+}
+
+$("#btnTodo").on('click', addTodo);
 
 $("#todoList").on("click", "li", (event) => {
   $(event.target).toggleClass("checked");
 });
+
+addTodo("Buy milk");

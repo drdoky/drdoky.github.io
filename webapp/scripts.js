1b041dee93bc53ed005575c0bb0987881f7bd83e
drdoky <drdoky@users.noreply.github.com>
2020-05-06 18:32:42 +0200

    img info upgrade

--- webapp/scripts.js
+++ webapp/scripts.js
@@ -1,4 +1,5 @@
 $(".green").on("click", () => {
   $(".actImg").toggleClass("aktiv inaktiv");
-  console.log("run");
+  console.log($(".aktiv").width());
+  $(".imgInfo").width($(".aktiv").width());
 });
